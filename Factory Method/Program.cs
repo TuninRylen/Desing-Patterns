3f40c56d68a471bec07ce0d29e914b6c58f19aae
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2023-10-03 19:14:02 +0300

    Update

---
+++ Factory Method/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager( new LoggerFactory2());
+            customerManager.Save();
+
+            Console.ReadLine();
+        } 
+    }
+
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new UgLogger();
+        }
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new AyLogger();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+   
+    public class UgLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with UgLogger");
+        }
+    }
+    public class AyLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with AyLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            //newleme operaysonunu minimalize hale getirdik. Bağımlılıklardan arındırıldı. Bu desing patternın amacı da bu zaten.
+            ILogger logger = _loggerFactory.CreateLogger(); 
+            logger.Log();
+        }
+    }
+
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+
+            customerManager.Save();
+        }
+
+        class CustomerManager
+        {
+            static CustomerManager _customerManager;
+            static object _locker = new object();
+        
+            private CustomerManager() { }
+
+            public static CustomerManager CreateAsSingleton()
+            {
+                lock (_locker)
+                {
+                    if (_customerManager == null)
+                    {
+                        _customerManager = new CustomerManager();
+                    }
+                }
+
+                return _customerManager;
+            }
+
+            public void Save()
+            {
+                Console.ReadLine();
+            }
+
+        }
+
+    }
+}
