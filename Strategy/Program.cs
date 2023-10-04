a9d0ab52397e58dea8292bb3e488a67f9be1563b
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2023-10-04 14:51:42 +0300

    UpdateV10

---
+++ Strategy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase = new AfterBefore2010CreditCalculator();
+            customerManager.SaveCredit();
+
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using before 2010");
+        }
+    }
+
+    class AfterBefore2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using after 2010");
+        }
+    }
+
+    class CustomerManager
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+
+        public void SaveCredit()
+        {
+            Console.WriteLine("Costomer manager business");
+            CreditCalculatorBase.Calculate();
+        }
+    }
+}
