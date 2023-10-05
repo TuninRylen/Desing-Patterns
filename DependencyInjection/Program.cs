2756146ef81820f139642cde696fa031c0c83437
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2023-10-05 18:22:10 +0300

    UpdateV15

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace DependencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Bind<IProductDal>().To<EfProductDal>().InSingletonScope(); ;
+
+            ProductManager productManager = new ProductManager(kernel.Get<IProductDal>());
+            productManager.Save();
+
+            Console.Read();
+        }
+    }
+
+
+    interface IProductDal
+    {
+        void Save();
+    }
+
+
+    class EfProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Ef");
+        }
+    }
+
+    class NhProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Nh");
+        }
+    }
+
+
+    class ProductManager
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productdal)
+        {
+            _productDal = productdal;
+        }
+        public void Save()
+        {
+            _productDal.Save();
+        }
+
+    }
+}
